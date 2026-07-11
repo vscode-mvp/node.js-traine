36da053245c55ec609196f4fba9c1d66da004259
vscode-mvp <procachprocach@gmail.com>
2026-07-11 21:12:46 +0700

    Добав лена работа с уникальными идентификаторами: 1) их сохранение в обьекте ids 2) дублирование всех идентификаторов в консоль. Other: 1)переработана логика: переход с require на современные import/export 2) зависимост и подтягиваются не напрямую, а из файла modules.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { generatedId } from "./modules.js";
+const ids = { id: null };
+
+function generateIdObj() {
+    for (let i = 0; i < 10; i++) {
+        let genId = generatedId()
+        ids.id = genId
+
+        console.log(`id ${i}: ${genId}`)
+    }
+}
+generateIdObj()
